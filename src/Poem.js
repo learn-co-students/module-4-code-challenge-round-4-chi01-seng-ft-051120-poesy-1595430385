72d4f8b605bbfbeb37e1699041883327a4a0c22e
jayastronomic <juliansmith94@hotmail.com>
2020-07-22 11:26:23 -0500

    I need more time to study

--- src/Poem.js
+++ src/Poem.js
@@ -4,7 +4,7 @@ class Poem extends React.Component {
   render() {
     return (
       <div>
-        <h3>Title</h3>
+        <h3>{this.props.poem.title}</h3>
         <p>Content</p>
         <p>
           <strong>- By Author</strong>
--- src/PoemsContainer.js
+++ src/PoemsContainer.js
@@ -2,12 +2,20 @@ import React from "react";
 import Poem from "./Poem";
 
 class PoemsContainer extends React.Component {
+
+  renderPoems = () => {
+    this.props.poems.map(poemObj => {
+      return <Poem poem={poemObj} />
+    })
+  }
+
+
   render() {
+
+
     return (
-      <div className="poems-container">
-        {
-          // render poems here
-        }
+      <div className="poems-container" poems={this.props.poems} >
+       {this.renderPoems()} 
       </div>
     );
   }
