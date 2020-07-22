6ebb49ac7c504e87ffe20dd5518f1870eff23884
sidjagarlamudi <jaglings@gmail.com>
2020-07-22 11:39:03 -0500

    done

--- src/App.js
+++ src/App.js
@@ -4,14 +4,59 @@ import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      poems: [],
+      display: false,
+    };
+  }
+
+  componentDidMount = () => {
+    this.fetchPoems();
+  };
+
+  fetchPoems = () => {
+    fetch("http://localhost:6001/poems")
+      .then((resp) => resp.json())
+      .then((data) =>
+        this.setState({
+          poems: data,
+        })
+      );
+  };
+
+  handleClick = () => {
+    let newBoolean = !this.state.display;
+    this.setState({
+      display: newBoolean,
+    });
+  };
+
+  formData = (newPoem) => {
+    const reqObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPoem),
+    };
+
+    fetch("http://localhost:6001/poems", reqObj)
+      .then((resp) => resp.json())
+      .then(() => {
+        this.fetchPoems();
+      });
+  };
+
   render() {
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onClick={this.handleClick}>Show/hide new poem form</button>
+          {this.state.display ? <NewPoemForm formData={this.formData} /> : null}
         </div>
-        <PoemsContainer />
+        <PoemsContainer poems={this.state.poems} />
       </div>
     );
   }
--- src/Poem.js
+++ src/Poem.js
@@ -1,15 +1,32 @@
 import React from "react";
 
 class Poem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      clicked: false,
+    };
+  }
+
+  // ran out of time. to toggle button, I would implement a ternary 
+
+  handleClick = () => {
+    let newBoolean = !this.state.clicked;
+    this.setState({
+      clicked: newBoolean,
+    });
+  };
+
   render() {
+    const { title, content, author } = this.props.poem;
     return (
       <div>
-        <h3>Title</h3>
-        <p>Content</p>
+        <h3>{title}</h3>
+        <p>{content}</p>
         <p>
-          <strong>- By Author</strong>
+          <strong>- By {author}</strong>
         </p>
-        <button>Mark as read</button>
+        <button onClick={this.handleClick} >Mark as read</button>
       </div>
     );
   }
